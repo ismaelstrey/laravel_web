6932b73ca4042373ebdc7e88d07707fe95cd5dae
ismael strey pereira <ismaelstrey@hotmail.com>
2019-04-25 00:06:52 -0300

    dev

--- resources/views/lista.blade.php
+++ resources/views/lista.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ContactMe</title>
+    <title>Area de contato</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
